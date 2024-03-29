d57d56fb45aaf238ec223ac1cc1c00e56b5be60d
shreyanshsing <shreyanshsinghjee@gmail.com>
2024-03-29 22:45:09 +0530

    fix: updated priorit-queue

--- data-structures/PriorityQueue.ts
+++ data-structures/PriorityQueue.ts
@@ -1,6 +1,13 @@
-import {ListNode} from './LinkedList'
+class ListNode<T> {
+    val: T
+    next: ListNode<T> | null
+    constructor(val: T) {
+        this.val = val
+        this.next = null
+    }
+}
 
-class PrioritQueue<T> {
+export default class PrioritQueue<T> {
     private head: ListNode<T> | null
     private tail: ListNode<T> | null
     private size: number
@@ -24,7 +31,6 @@ class PrioritQueue<T> {
     }
 
     append(val: T) {
-        
         const temp = new ListNode(val)
         if(this.size === 0) {
             this.head = temp
